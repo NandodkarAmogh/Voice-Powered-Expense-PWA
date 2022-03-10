614f60bd6c2bdeefdbea108bff31186726f6c0d6
Amogh Nandodkar <nandodkaramogh@gmail.com>
2022-03-10 19:38:25 +0530

    Speechly setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Details from './components/Details/Details';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButtonContainer, PushToTalkButton } from '@speechly/react-ui';
 import useStyles from './styles';
 import Main from './components/Main/Main';
 
@@ -9,16 +10,19 @@ const App = () => {
     return (
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems='center' justify='center' style={{height: '100vh'}}>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={11} sm={4}>
                     <Details title="Income"/>
                 </Grid>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={11} sm={3}>
                     <Main />
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={11} sm={4}>
                     <Details title="Expense" />
                 </Grid>
             </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+            </PushToTalkButtonContainer>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from "@speechly/react-client";
 
 import { Provider } from "./context/context";
 import App from './App';
-import './index.css'
+import './index.css';
+
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId="4bccb007-2a2a-4f7c-9d3a-575db953ff59" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
  );
